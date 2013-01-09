9a4174389b36eebc9f142f20ea528a4d4fe13939
DHJ <github@h-jay.com>
2013-01-09 16:20:59 +0000

    REL: up to 3.0.4

--- default.py
+++ default.py
@@ -48,7 +48,7 @@ __cwd__ = __settings__.getAddonInfo('path')
 BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
 PLUGINPATH=xbmc.translatePath( os.path.join( __cwd__) )
 sys.path.append(BASE_RESOURCE_PATH)
-PLEXBMC_VERSION="3.0.3"
+PLEXBMC_VERSION="3.0.4"
 
 try:
     from bonjourFind import *
