c06f8ad0723bda70f6406ffa88b5aa281b37ac64
DHJ <github@h-jay.com>
2013-04-03 20:24:19 +0100

    ADD: Activate sort by sort title.

--- plexbmc.py
+++ plexbmc.py
@@ -2374,7 +2374,7 @@ def getContent( url ):
     if view_group == "movie":
         printDebug( "This is movie XML, passing to Movies")
         if not (lastbit.startswith('recently') or lastbit.startswith('newest')):
-            xbmcplugin.addSortMethod(pluginhandle,xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+            xbmcplugin.addSortMethod(pluginhandle,xbmcplugin.SORT_METHOD_VIDEO_SORT_TITLE_IGNORE_THE )
         Movies(url, tree)
     elif view_group == "show":
         printDebug( "This is tv show XML")
@@ -4285,7 +4285,7 @@ else:
         TVShows(param_url)
 
     elif mode == _MODE_MOVIES:
-        xbmcplugin.addSortMethod(pluginhandle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE )
+        xbmcplugin.addSortMethod(pluginhandle, xbmcplugin.SORT_METHOD_VIDEO_SORT_TITLE_IGNORE_THE  )
         Movies(param_url)
 
     elif mode == _MODE_ARTISTS:
