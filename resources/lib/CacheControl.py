1ffb2c3802bd3a80db3f9d7ccea5253155fd6919
hippojay <plexbmc@h-jay.com>
2015-03-11 16:56:01 +0000

    migrate to common printdebug for cachecontrol
    reduce complexity

--- resources/lib/CacheControl.py
+++ resources/lib/CacheControl.py
@@ -18,56 +18,52 @@ except ImportError:
     
 from common import *
     
+printDebug=printDebug("PleXBMC", "cachecontrol")    
+    
 class CacheControl:
 
-    def __init__(self, cache_location, debug=0, enabled=True):
+    def __init__(self, cache_location, enabled=True):
     
-        self.cache_location=cache_location
-        
-        self.__printDebug=printDebug("PleXBMC", "cachecontrol")
-        
-        self.DEBUG_OFF=0
-        self.DEBUG_INFO=1
-        self.DEBUG_DEBUG=2
-        self.DEBUG_DEBUGPLUS=3
-        
-        self.debug=debug
+        self.cache_location=cache_location        
         self.enabled=enabled
 
         if self.enabled:
         
+
+            if not xbmcvfs.exists(self.cache_location):
+                printDebug.debug("CACHE [%s]: Location does not exist.  Creating" % self.cache_location)
+                if not xbmcvfs.mkdirs(self.cache_location):
+                    printDebug.debug("CACHE [%s]: Location cannot be created" % self.cache_location)
+                    self.cache_location=None
+                    return
+            printDebug.debug("Running with cache location: %s" % self.cache_location)
+
             if self.cache_location[-1] != "/":
                 self.cache_location+="/"
 
-            if not xbmcvfs.exists(self.cache_location):
-                if not xbmcvfs.mkdir(self.cache_location):
-                    self.__printDebug("CACHE [%s]: Location does not exist" % self.cache_location)
-                    self.cache_location=None
-            self.__printDebug("Running with cache location: %s" % self.cache_location)
-
         else:
             self.cache_location=None
-            self.__printDebug("Cache is disabled")
+            printDebug.debug("Cache is disabled")
                 
     def readCache(self, cache_name):
         if self.cache_location is None:
             return (False, None)
 
-        self.__printDebug("CACHE [%s]: attempting to read" % cache_name)
+        printDebug.debug("CACHE [%s]: attempting to read" % cache_name)
         try:
             cache=xbmcvfs.File(self.cache_location+cache_name)
             cachedata = cache.read()
             cache.close()
         except Exception, e:
-            self.__printDebug("CACHE [%s]: read error [%s]" % ( cache_name, e))
+            printDebug.debug("CACHE [%s]: read error [%s]" % ( cache_name, e))
         
         if cachedata:
-            self.__printDebug("CACHE [%s]: read" % cache_name)
-            self.__printDebug("CACHE [%s]: data: [%s]" % ( cache_name, cachedata), level=self.DEBUG_DEBUGPLUS)
+            printDebug.debug("CACHE [%s]: read" % cache_name)
+            printDebug.debugplus("CACHE [%s]: data: [%s]" % ( cache_name, cachedata))
             cacheobject = pickle.loads(cachedata)
             return (True, cacheobject)
 
-        self.__printDebug("CACHE [%s]: empty" % cache_name)
+        printDebug.debug("CACHE [%s]: empty" % cache_name)
         return (False, None)
 
     def writeCache(self,cache_name,object):
@@ -75,13 +71,13 @@ class CacheControl:
         if self.cache_location is None:
             return True
 
-        self.__printDebug("CACHE [%s]: Writing file" % cache_name)
+        printDebug.debug("CACHE [%s]: Writing file" % cache_name)
         try:
             cache=xbmcvfs.File(self.cache_location+cache_name,'w')
             cache.write(pickle.dumps(object))
             cache.close()
         except Exception, e:
-            self.__printDebug("CACHE [%s]: Writing error [%s]" % (self.cache_location+cache_name, e))
+            printDebug.debug("CACHE [%s]: Writing error [%s]" % (self.cache_location+cache_name, e))
             
             
         return True
@@ -92,34 +88,34 @@ class CacheControl:
             return (False, None)
 
         if xbmcvfs.exists(self.cache_location+cache_name):
-            self.__printDebug("CACHE [%s]: exists" % cache_name)
+            printDebug.debug("CACHE [%s]: exists" % cache_name)
             now = int(round(time.time(),0))
             modified = int(xbmcvfs.Stat(self.cache_location+cache_name).st_mtime())
-            self.__printDebug ("CACHE [%s]: mod[%s] now[%s] diff[%s]" % ( cache_name, modified, now, now - modified))
+            printDebug.debug ("CACHE [%s]: mod[%s] now[%s] diff[%s]" % ( cache_name, modified, now, now - modified))
 
             if ( modified < 0) or ( now - modified ) > life:
-                self.__printDebug("CACHE [%s]: too old, delete" % cache_name)
+                printDebug.debug("CACHE [%s]: too old, delete" % cache_name)
                 
                 if xbmcvfs.delete(self.cache_location+cache_name):
-                    self.__printDebug("CACHE [%s]: deleted" % cache_name)
+                    printDebug.debug("CACHE [%s]: deleted" % cache_name)
                 else:
-                    self.__printDebug("CACHE [%s]: not deleted" % cache_name)
+                    printDebug.debug("CACHE [%s]: not deleted" % cache_name)
             else:
-                self.__printDebug("CACHE [%s]: current" % cache_name)
+                printDebug.debug("CACHE [%s]: current" % cache_name)
 
                 return self.readCache(cache_name)
         else:
-            self.__printDebug("CACHE [%s]: does not exist" % cache_name)
+            printDebug.debug("CACHE [%s]: does not exist" % cache_name)
 
         return (False, None)
         
     def deleteCache(self):
-        self.__printDebug("== ENTER: deleteCache ==")
+        printDebug.debug("== ENTER: deleteCache ==")
         cache_suffix = "cache"
         dirs, files = xbmcvfs.listdir(self.cache_location)
 
-        self.__printDebug("List of file: [%s]" % files)
-        self.__printDebug("List of dirs: [%s]" % dirs)
+        printDebug.debug("List of file: [%s]" % files)
+        printDebug.debug("List of dirs: [%s]" % dirs)
         
         for i in files:
 
@@ -127,7 +123,7 @@ class CacheControl:
                 continue
 
             if xbmcvfs.delete(self.cache_location+i):
-                self.__printDebug("SUCCESSFUL: removed %s" % i)
+                printDebug.debug("SUCCESSFUL: removed %s" % i)
             else:
-                self.__printDebug("UNSUCESSFUL: did not remove %s" % i )
+                printDebug.debug("UNSUCESSFUL: did not remove %s" % i )
         
