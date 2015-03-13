059742f52582fbaa016e17435d083d6cc72b2902
hippojay <plexbmc@h-jay.com>
2015-03-13 12:15:50 +0000

    tidy up output

--- default.py
+++ default.py
@@ -1,13 +1,15 @@
 
 import xbmcaddon
 import os
+
+print "===== PLEXBMC START ====="
+
 from common import *  #Needed first to setup import locations
 import plexbmc
 import time
 
 plexbmc_start = time.time()
 
-print "===== PLEXBMC START ====="
 plexbmc.start_plexbmc()
 print "===== PLEXBMC STOP: %s seconds =====" % ( time.time() - plexbmc_start )
 
