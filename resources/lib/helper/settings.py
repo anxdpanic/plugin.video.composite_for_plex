48e190b43b5a8b4990b0c0910554ef3737439e1e
DHJ <dhj@h-jay.com>
2015-11-21 19:14:52 +0000

    Merge client name/uuid settings

--- resources/lib/helper/settings.py
+++ resources/lib/helper/settings.py
@@ -9,7 +9,7 @@ try:
 except:
     print "Unable to read XBMC's guisettings.xml"    
 
-def getGUI(name):
+def get_kodi_setting(name):
     global guidoc
     if guidoc is None:
         return False
@@ -19,24 +19,23 @@ def getGUI(name):
         return ""
 
 addon = xbmcaddon.Addon()
-plexbmc = xbmcaddon.Addon('plugin.video.plexbmc')
 
 settings['debug'] = addon.getSetting('debug') == "true"
 settings['gdm_debug'] = addon.getSetting('gdm_debug') == "true"
 if addon.getSetting('use_xbmc_name') == "true":
-    settings['client_name'] = getGUI('devicename')
+    settings['client_name'] = get_kodi_setting('devicename')
 else:
-    settings['client_name'] = addon.getSetting('c_name')
-# XBMC web server settings
-settings['webserver_enabled'] = (getGUI('webserver') == "true")
-settings['port'] = int(getGUI('webserverport'))
-settings['user'] = getGUI('webserverusername')
-settings['passwd'] = getGUI('webserverpassword')
+    settings['client_name'] = addon.getSetting('devicename')
+# KODI web server settings
+settings['webserver_enabled'] = (get_kodi_setting('webserver') == "true")
+settings['port'] = int(get_kodi_setting('webserverport'))
+settings['user'] = get_kodi_setting('webserverusername')
+settings['passwd'] = get_kodi_setting('webserverpassword')
 
-settings['uuid'] = str(addon.getSetting('uuid')) or str(uuid.uuid4())
-addon.setSetting('uuid', settings['uuid'])
+settings['uuid'] = str(addon.getSetting('client_id')) or str(uuid.uuid4())
+addon.setSetting('uuid', settings['client_id'])
 settings['version'] = addon.getAddonInfo('version')
-settings['plexbmc_version'] = plexbmc.getAddonInfo('version')
-settings['myplex_user'] = plexbmc.getSetting('myplex_user')
+settings['plexbmc_version'] = addon.getAddonInfo('version')
+settings['myplex_user'] = addon.getSetting('myplex_user')
 settings['serverList'] = []
 settings['myport'] = 3005
