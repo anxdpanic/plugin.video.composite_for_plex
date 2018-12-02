f5d4fdd7ffed3c67147a1188a06226cfcc8640aa
anxdpanic <anxdpanic@users.noreply.github.com>
2018-12-02 13:06:15 -0500

    simplify entry point

--- resources/lib/entry_point.py
+++ resources/lib/entry_point.py
@@ -4,28 +4,9 @@ import time
 import xbmc
 
 bplex_start = time.time()
+
 from bplex_addon import bplex
 
-profile = False
+bplex.start_bplex(sys.argv)
 
-if not profile:
-    xbmc.log('bPlex -> Script argument is %s' % sys.argv, xbmc.LOGDEBUG)
-    bplex.start_bplex(sys.argv)
-else:
-    # Enable extra profile information
-    import StringIO
-    import cProfile
-    import pstats
-
-    cProfile.run('bplex.start_bplex()', 'statsfile')
-    stream = StringIO.StringIO()
-    stats = pstats.Stats('statsfile', stream=stream)
-    stats.sort_stats('tottime')
-    stats.print_stats()
-
-    # Stream now contains the report text.
-    # Can be accessed with stream.getvalue()
-
-    xbmc.log(stream.getvalue(), xbmc.LOGDEBUG)
-
-xbmc.log('bPlex STOP [id: %s]: %s seconds' % (bplex_start, (time.time() - bplex_start)), xbmc.LOGDEBUG)
+xbmc.log('bPlex finished. |%ss|' % (time.time() - bplex_start), xbmc.LOGDEBUG)
