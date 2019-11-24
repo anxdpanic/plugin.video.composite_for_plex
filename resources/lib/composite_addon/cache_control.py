01bf52a84c19c3ad03005e8fc46308cc5d1a0bb1
anxdpanic <anxdpanic@gmail.com>
2019-11-24 12:26:23 -0500

    [cache_control] linting

--- resources/lib/composite_addon/cache_control.py
+++ resources/lib/composite_addon/cache_control.py
@@ -15,16 +15,16 @@
 
 import time
 
-import xbmcvfs
-
 from six import PY3
 # noinspection PyPep8Naming
 from six.moves import cPickle as pickle
 
+import xbmcvfs  # pylint: disable=import-error
+
 from .common import CONFIG
 from .common import PrintDebug
 
-log_print = PrintDebug(CONFIG['name'], 'cachecontrol')
+LOG = PrintDebug(CONFIG['name'], 'cachecontrol')
 
 
 class CacheControl:
@@ -41,43 +41,43 @@ class CacheControl:
                 self.cache_location += delim
 
             if not xbmcvfs.exists(self.cache_location):
-                log_print.debug('CACHE [%s]: Location does not exist.  Creating' %
-                                self.cache_location)
+                LOG.debug('CACHE [%s]: Location does not exist.  Creating' %
+                          self.cache_location)
                 if not xbmcvfs.mkdirs(self.cache_location):
-                    log_print.debug('CACHE [%s]: Location cannot be created' %
-                                    self.cache_location)
+                    LOG.debug('CACHE [%s]: Location cannot be created' %
+                              self.cache_location)
                     self.cache_location = None
                     return
-            log_print.debug('Running with cache location: %s' % self.cache_location)
+            LOG.debug('Running with cache location: %s' % self.cache_location)
 
         else:
             self.cache_location = None
-            log_print.debug('Cache is disabled')
+            LOG.debug('Cache is disabled')
 
     def read_cache(self, cache_name):
         if self.cache_location is None:
             return False, None
 
-        log_print.debug('CACHE [%s]: attempting to read' % cache_name)
+        LOG.debug('CACHE [%s]: attempting to read' % cache_name)
         cache = xbmcvfs.File(self.cache_location + cache_name)
         try:
             if PY3:
                 cachedata = cache.readBytes()
             else:
                 cachedata = cache.read()
-        except Exception as e:
-            log_print.debug('CACHE [%s]: read error [%s]' % (cache_name, e))
+        except Exception as error:  # pylint: disable=broad-except
+            LOG.debug('CACHE [%s]: read error [%s]' % (cache_name, error))
             cachedata = False
         finally:
             cache.close()
 
         if cachedata:
-            log_print.debug('CACHE [%s]: read' % cache_name)
-            log_print.debugplus('CACHE [%s]: data: [%s]' % (cache_name, cachedata))
+            LOG.debug('CACHE [%s]: read' % cache_name)
+            LOG.debugplus('CACHE [%s]: data: [%s]' % (cache_name, cachedata))
             cacheobject = pickle.loads(cachedata)
             return True, cacheobject
 
-        log_print.debug('CACHE [%s]: empty' % cache_name)
+        LOG.debug('CACHE [%s]: empty' % cache_name)
         return False, None
 
     def write_cache(self, cache_name, obj):
@@ -85,16 +85,16 @@ class CacheControl:
         if self.cache_location is None:
             return True
 
-        log_print.debug('CACHE [%s]: Writing file' % cache_name)
+        LOG.debug('CACHE [%s]: Writing file' % cache_name)
         cache = xbmcvfs.File(self.cache_location + cache_name, 'w')
         try:
             if PY3:
                 cache.write(bytearray(pickle.dumps(obj)))
             else:
                 cache.write(pickle.dumps(obj))
-        except Exception as e:
-            log_print.debug('CACHE [%s]: Writing error [%s]' %
-                            (self.cache_location + cache_name, e))
+        except Exception as error:  # pylint: disable=broad-except
+            LOG.debug('CACHE [%s]: Writing error [%s]' %
+                      (self.cache_location + cache_name, error))
         finally:
             cache.close()
         return True
@@ -104,19 +104,18 @@ class CacheControl:
             return None
 
         if xbmcvfs.exists(self.cache_location + cache_name):
-            log_print.debug('CACHE [%s]: exists, ttl: |%s|' % (cache_name, str(ttl)))
+            LOG.debug('CACHE [%s]: exists, ttl: |%s|' % (cache_name, str(ttl)))
             now = int(round(time.time(), 0))
             modified = int(xbmcvfs.Stat(self.cache_location + cache_name).st_mtime())
-            log_print.debug('CACHE [%s]: mod[%s] now[%s] diff[%s]' %
-                            (cache_name, modified, now, now - modified))
+            LOG.debug('CACHE [%s]: mod[%s] now[%s] diff[%s]' %
+                      (cache_name, modified, now, now - modified))
 
             if (modified < 0) or (now - modified) > ttl:
                 return False
 
             return True
-        else:
-            log_print.debug('CACHE [%s]: does not exist' % cache_name)
 
+        LOG.debug('CACHE [%s]: does not exist' % cache_name)
         return None
 
     def check_cache(self, cache_name, ttl=3600):
@@ -126,14 +125,14 @@ class CacheControl:
         cache_valid = self.is_valid(cache_name, ttl)
 
         if cache_valid is False:
-            log_print.debug('CACHE [%s]: too old, delete' % cache_name)
+            LOG.debug('CACHE [%s]: too old, delete' % cache_name)
             if xbmcvfs.delete(self.cache_location + cache_name):
-                log_print.debug('CACHE [%s]: deleted' % cache_name)
+                LOG.debug('CACHE [%s]: deleted' % cache_name)
             else:
-                log_print.debug('CACHE [%s]: not deleted' % cache_name)
+                LOG.debug('CACHE [%s]: not deleted' % cache_name)
 
         elif cache_valid:
-            log_print.debug('CACHE [%s]: current' % cache_name)
+            LOG.debug('CACHE [%s]: current' % cache_name)
             return self.read_cache(cache_name)
 
         return False, None
@@ -143,17 +142,17 @@ class CacheControl:
         persistant_cache_suffix = '.pcache'
         dirs, file_list = xbmcvfs.listdir(self.cache_location)
 
-        log_print.debug('List of file: [%s]' % file_list)
-        log_print.debug('List of dirs: [%s]' % dirs)
+        LOG.debug('List of file: [%s]' % file_list)
+        LOG.debug('List of dirs: [%s]' % dirs)
 
-        for f in file_list:
+        for cache_file in file_list:
 
-            if force and persistant_cache_suffix in f:
-                log_print.debug('Force deletion of persistent cache file')
-            elif cache_suffix not in f:
+            if force and persistant_cache_suffix in cache_file:
+                LOG.debug('Force deletion of persistent cache file')
+            elif cache_suffix not in cache_file:
                 continue
 
-            if xbmcvfs.delete(self.cache_location + f):
-                log_print.debug('SUCCESSFUL: removed %s' % f)
+            if xbmcvfs.delete(self.cache_location + cache_file):
+                LOG.debug('SUCCESSFUL: removed %s' % cache_file)
             else:
-                log_print.debug('UNSUCESSFUL: did not remove %s' % f)
+                LOG.debug('UNSUCESSFUL: did not remove %s' % cache_file)
